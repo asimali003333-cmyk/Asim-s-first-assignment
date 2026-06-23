8a29cf2bb419a0b194908e1d23ce97bf65041ab5
Asim Ali Shaikh 4545 <asimali003333@gmail.com>
2026-06-23 22:35:36 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
+var name = "Asim";
+var age = 16;
 
-// first Method
-let meratable = prompt("enter your table number");
-let run = prompt("run your table number");
-for (let i = 1; i < run; i++) {
-    console.log(i)   
-}
+console.log(name);
+console.log(age);
 
-
-
-
-
-// second method
-     for (let i = 1; i < run; i++) {
-        document.onwebkittransitionend(i)// space create
-        console.log
-     }
-
-
-
-
-
-     // // third method
-     for (let i = 1; i < 10; i++){
-        console.log(meratable + "x" + i + "=" + meratable * i)
-        console.log(5 + "x" + i + "=" + 5 * i);
-
-     }
-
-
-
-
-     // fourth method 
-     for (let i = 1; i <= 5; i++) {
-        console.log('${2} x ${i} = ${2 * i}');
-     }
+console.log("my name is " + name);
+console.log("my age is " + age);
